8c9d71d7e3b8b346faa88c23c81e8ba556391be2
Shamsiddinxon <nosirovshamsiddinxon@gmail.com>
2022-01-29 16:50:32 +0500

    responsiv

--- js/main.js
+++ js/main.js
@@ -3,4 +3,21 @@ window.addEventListener('DOMContentLoaded', function() {
         const header = document.querySelector('header');
         header.classList.toggle('sticky', window.scrollY > 0)
     })
+
+    const menuBtn = document.querySelector(".menu__btn");
+    const navigation = document.querySelector(".nav")
+    const navItems = document.querySelector(".nav a")
+
+    menuBtn.addEventListener("click", () => {
+        menuBtn.classList.toggle("active");
+        navigation.classList.toggle("active");
+    })
+
+    console.log(navItems);
+
+        navItems.addEventListener("click", () => {
+            menuBtn.classList.remove("active");
+            navItems.classList.remove("active");
+        })
+
 })
